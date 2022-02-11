0173a5802941743489b41a0140b473744aeefbb8
Kyle <kyleatyahoo@gmail.com>
2022-02-11 18:57:31 -0500

    fix print statement

--- hw4/main.c
+++ hw4/main.c
@@ -5,13 +5,35 @@
 
 extern uint64_t our_code_starts_here() asm("our_code_starts_here");
 extern uint64_t print(uint64_t val) asm("print");
+const NUM_TAG_MASK = 0x0000000000000001L;
+const TRUE = 0xFFFFFFFFFFFFFFFFL;
+const FALSE = 0x7FFFFFFFFFFFFFFFL;
 
-uint64_t print(uint64_t val) {
-  printf("Unknown value: %#018x\n", val);
+uint64_t print(uint64_t val)
+{
+  // Number
+  if ((NUM_TAG_MASK ^ val) & 1 == 1)
+  {
+    printf("%n", val);
+  }
+  else if (val == TRUE)
+  {
+    printf("true");
+  }
+  else if (val == FALSE)
+  {
+    printf("false");
+  }
+  else
+  {
+    printf("Unknown value: %#018x\n", val);
+  }
+
   return val;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   uint64_t result = our_code_starts_here();
   print(result);
   return 0;
