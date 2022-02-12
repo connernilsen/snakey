bdc316ca4f4297213765dab3ec8fa829c0d595bb
Kyle <kyleatyahoo@gmail.com>
2022-02-11 18:25:47 -0600

    format

--- hw4/compile.ml
+++ hw4/compile.ml
@@ -9,8 +9,8 @@ let rec is_anf (e : 'a expr) : bool =
   | EPrim1(_, e, _) -> is_imm e
   | EPrim2(_, e1, e2, _) -> is_imm e1 && is_imm e2
   | ELet(binds, body, _) ->
-     List.for_all (fun (_, e, _) -> is_anf e) binds
-     && is_anf body
+    List.for_all (fun (_, e, _) -> is_anf e) binds
+    && is_anf body
   | EIf(cond, thn, els, _) -> is_imm cond && is_anf thn && is_anf els
   | _ -> is_imm e
 and is_imm e =
@@ -45,25 +45,25 @@ let check_scope (e : sourcespan expr) : sourcespan expr =
     | EBool _ -> ()
     | ENumber _ -> ()
     | EId (x, loc) ->
-       (try ignore (List.assoc x env)
-        with Not_found ->
-             raise (BindingError(sprintf "The identifier %s, used at <%s>, is not in scope" x (string_of_sourcespan loc))))
+      (try ignore (List.assoc x env)
+       with Not_found ->
+         raise (BindingError(sprintf "The identifier %s, used at <%s>, is not in scope" x (string_of_sourcespan loc))))
     | EPrim1(_, e, _) -> help e env
     | EPrim2(_, l, r, _) -> help l env; help r env
     | EIf(c, t, f, _) -> help c env; help t env; help f env
     | ELet(binds, body, _) ->
-       let (env2, _) =
-         (List.fold_left
+      let (env2, _) =
+        (List.fold_left
            (fun (scope_env, shadow_env) (x, e, loc) ->
-             try
-               let existing = List.assoc x shadow_env in
-               raise (BindingError(sprintf "The identifier %s, defined at <%s>, shadows one defined at <%s>"
-                                           x (string_of_sourcespan loc) (string_of_sourcespan existing)))
-             with Not_found ->
-               help e scope_env;
-               ((x, loc)::scope_env, (x, loc)::shadow_env))
+              try
+                let existing = List.assoc x shadow_env in
+                raise (BindingError(sprintf "The identifier %s, defined at <%s>, shadows one defined at <%s>"
+                                      x (string_of_sourcespan loc) (string_of_sourcespan existing)))
+              with Not_found ->
+                help e scope_env;
+                ((x, loc)::scope_env, (x, loc)::shadow_env))
            (env, []) binds) in
-       help body env2
+      help body env2
   in help e []; e
 
 let rename (e : tag expr) : tag expr =
@@ -77,28 +77,28 @@ let tag (e : 'a expr) : tag expr =
     | ENumber(n, _) -> (ENumber(n, num), num + 1)
     | EBool(b, _) -> (EBool(b, num), num + 1)
     | EPrim1(op, e, _) ->
-       let (tag_e, new_n) = help e (num + 1) in
-       (EPrim1(op, tag_e, num), new_n)
+      let (tag_e, new_n) = help e (num + 1) in
+      (EPrim1(op, tag_e, num), new_n)
     | EPrim2(op, e1, e2, _) ->
-       let (tag_e1, num_e1) = help e1 (num + 1) in
-       let (tag_e2, num_e2) = help e2 (num_e1) in
-       (EPrim2(op, tag_e1, tag_e2, num), num_e2)
+      let (tag_e1, num_e1) = help e1 (num + 1) in
+      let (tag_e2, num_e2) = help e2 (num_e1) in
+      (EPrim2(op, tag_e1, tag_e2, num), num_e2)
     | ELet(binds, body, _) ->
-       let (new_binds, num_binds) =
-         List.fold_left
-           (fun (rev_binds, next_num) (x, b, _) ->
+      let (new_binds, num_binds) =
+        List.fold_left
+          (fun (rev_binds, next_num) (x, b, _) ->
              let (tag_b, num_b) = help b (next_num + 1) in
              ((x, tag_b, next_num)::rev_binds, num_b))
-           ([], num + 1) binds in
-       let (tag_body, num_body) = help body num_binds in
-       (ELet(List.rev new_binds, tag_body, num), num_body)
+          ([], num + 1) binds in
+      let (tag_body, num_body) = help body num_binds in
+      (ELet(List.rev new_binds, tag_body, num), num_body)
     | EIf(cond, thn, els, _) ->
-       let (tag_cond, num_cond) = help cond (num + 1) in
-       let (tag_thn, num_thn) = help thn (num_cond) in
-       let (tag_els, num_els) = help els (num_thn) in
-       (EIf(tag_cond, tag_thn, tag_els, num), num_els)
+      let (tag_cond, num_cond) = help cond (num + 1) in
+      let (tag_thn, num_thn) = help thn (num_cond) in
+      let (tag_els, num_els) = help els (num_thn) in
+      (EIf(tag_cond, tag_thn, tag_els, num), num_els)
   in let (ans, _) = help e 1
-     in ans
+  in ans
 
 let rec untag (e : 'a expr) : unit expr =
   match e with
@@ -106,57 +106,57 @@ let rec untag (e : 'a expr) : unit expr =
   | ENumber(n, _) -> ENumber(n, ())
   | EBool(b, _) -> EBool(b, ())
   | EPrim1(op, e, _) ->
-     EPrim1(op, untag e, ())
+    EPrim1(op, untag e, ())
   | EPrim2(op, e1, e2, _) ->
-     EPrim2(op, untag e1, untag e2, ())
+    EPrim2(op, untag e1, untag e2, ())
   | ELet(binds, body, _) ->
-     ELet(List.map(fun (x, b, _) -> (x, untag b, ())) binds, untag body, ())
+    ELet(List.map(fun (x, b, _) -> (x, untag b, ())) binds, untag body, ())
   | EIf(cond, thn, els, _) ->
-     EIf(untag cond, untag thn, untag els, ())
+    EIf(untag cond, untag thn, untag els, ())
 
 let anf (e : tag expr) : unit expr =
   let rec helpC (e : tag expr) : (unit expr * (string * unit expr) list) = 
     match e with
     | EPrim1(op, arg, _) ->
-       let (arg_imm, arg_setup) = helpI arg in
-       (EPrim1(op, arg_imm, ()), arg_setup)
+      let (arg_imm, arg_setup) = helpI arg in
+      (EPrim1(op, arg_imm, ()), arg_setup)
     | EPrim2(op, left, right, _) ->
-       let (left_imm, left_setup) = helpI left in
-       let (right_imm, right_setup) = helpI right in
-       (EPrim2(op, left_imm, right_imm, ()), left_setup @ right_setup)
+      let (left_imm, left_setup) = helpI left in
+      let (right_imm, right_setup) = helpI right in
+      (EPrim2(op, left_imm, right_imm, ()), left_setup @ right_setup)
     | EIf(cond, _then, _else, _) ->
-       let (cond_imm, cond_setup) = helpI cond in
-       (EIf(cond_imm, anf _then, anf _else, ()), cond_setup)
+      let (cond_imm, cond_setup) = helpI cond in
+      (EIf(cond_imm, anf _then, anf _else, ()), cond_setup)
     | ENumber(n, _) -> (ENumber(n, ()), [])
     | EBool(b, _) -> (EBool(b, ()), [])
     | ELet([], body, _) -> helpC body
     | ELet((bind, exp, _)::rest, body, pos) ->
-       let (exp_ans, exp_setup) = helpC exp in
-       let (body_ans, body_setup) = helpC (ELet(rest, body, pos)) in
-       (body_ans, exp_setup @ [(bind, exp_ans)] @ body_setup)
+      let (exp_ans, exp_setup) = helpC exp in
+      let (body_ans, body_setup) = helpC (ELet(rest, body, pos)) in
+      (body_ans, exp_setup @ [(bind, exp_ans)] @ body_setup)
     | EId(name, _) -> (EId(name, ()), [])
   and helpI (e : tag expr) : (unit expr * (string * unit expr) list) =
     match e with
     | EPrim1(op, arg, tag) ->
-       let tmp = sprintf "unary_%d" tag in
-       let (arg_imm, arg_setup) = helpI arg in
-       (EId(tmp, ()), arg_setup @ [(tmp, EPrim1(op, arg_imm, ()))])
+      let tmp = sprintf "unary_%d" tag in
+      let (arg_imm, arg_setup) = helpI arg in
+      (EId(tmp, ()), arg_setup @ [(tmp, EPrim1(op, arg_imm, ()))])
     | EPrim2(op, left, right, tag) ->
-       let tmp = sprintf "binop_%d" tag in
-       let (left_imm, left_setup) = helpI left in
-       let (right_imm, right_setup) = helpI right in
-       (EId(tmp, ()), left_setup @ right_setup @ [(tmp, EPrim2(op, left_imm, right_imm, ()))])
+      let tmp = sprintf "binop_%d" tag in
+      let (left_imm, left_setup) = helpI left in
+      let (right_imm, right_setup) = helpI right in
+      (EId(tmp, ()), left_setup @ right_setup @ [(tmp, EPrim2(op, left_imm, right_imm, ()))])
     | EIf(cond, _then, _else, tag) ->
-       let tmp = sprintf "if_%d" tag in
-       let (cond_imm, cond_setup) = helpI cond in
-       (EId(tmp, ()), cond_setup @ [(tmp, EIf(cond_imm, anf _then, anf _else, ()))])
+      let tmp = sprintf "if_%d" tag in
+      let (cond_imm, cond_setup) = helpI cond in
+      (EId(tmp, ()), cond_setup @ [(tmp, EIf(cond_imm, anf _then, anf _else, ()))])
     | ENumber(n, _) -> (ENumber(n, ()), [])
     | EBool(b, _) -> (EBool(b, ()), [])
     | ELet([], body, _) -> helpI body
     | ELet((bind, exp, _)::rest, body, pos) ->
-       let (exp_ans, exp_setup) = helpC exp in
-       let (body_ans, body_setup) = helpI (ELet(rest, body, pos)) in
-       (body_ans, exp_setup @ [(bind, exp_ans)] @ body_setup)
+      let (exp_ans, exp_setup) = helpC exp in
+      let (body_ans, body_setup) = helpI (ELet(rest, body, pos)) in
+      (body_ans, exp_setup @ [(bind, exp_ans)] @ body_setup)
     | EId(name, _) -> (EId(name, ()), [])
   and anf e = 
     let (ans, ans_setup) = helpI e in
@@ -165,7 +165,7 @@ let anf (e : tag expr) : unit expr =
   anf e
 ;;
 
-  
+
 let r_to_asm (r : reg) : string =
   match r with
   | RAX -> "RAX"
@@ -184,75 +184,75 @@ let rec arg_to_asm (a : arg) : string =
   | HexConst(n) -> sprintf "QWORD 0x%Lx" n
   | Reg(r) -> r_to_asm r
   | RegOffset(n, r) ->
-     if n >= 0 then
-       sprintf "[%s+%d]" (r_to_asm r) (word_size * n)
-     else
-       sprintf "[%s-%d]" (r_to_asm r) (-1 * word_size * n)
+    if n >= 0 then
+      sprintf "[%s+%d]" (r_to_asm r) (word_size * n)
+    else
+      sprintf "[%s-%d]" (r_to_asm r) (-1 * word_size * n)
   | Sized(size, a) ->
-     sprintf "%s %s"
-             (match size with | QWORD_PTR -> "QWORD" | DWORD_PTR -> "DWORD" | WORD_PTR -> "WORD" | BYTE_PTR -> "BYTE")
-             (arg_to_asm a)
+    sprintf "%s %s"
+      (match size with | QWORD_PTR -> "QWORD" | DWORD_PTR -> "DWORD" | WORD_PTR -> "WORD" | BYTE_PTR -> "BYTE")
+      (arg_to_asm a)
 
 let rec i_to_asm (i : instruction) : string =
   match i with
   | IMov(dest, value) ->
-     sprintf "  mov %s, %s" (arg_to_asm dest) (arg_to_asm value)
+    sprintf "  mov %s, %s" (arg_to_asm dest) (arg_to_asm value)
   | IAdd(dest, to_add) ->
-     sprintf "  add %s, %s" (arg_to_asm dest) (arg_to_asm to_add)
+    sprintf "  add %s, %s" (arg_to_asm dest) (arg_to_asm to_add)
   | ISub(dest, to_sub) ->
-     sprintf "  sub %s, %s" (arg_to_asm dest) (arg_to_asm to_sub)
+    sprintf "  sub %s, %s" (arg_to_asm dest) (arg_to_asm to_sub)
   | IMul(dest, to_mul) ->
-     sprintf "  imul %s, %s" (arg_to_asm dest) (arg_to_asm to_mul)
+    sprintf "  imul %s, %s" (arg_to_asm dest) (arg_to_asm to_mul)
   | ICmp(left, right) ->
-     sprintf "  cmp %s, %s" (arg_to_asm left) (arg_to_asm right)
+    sprintf "  cmp %s, %s" (arg_to_asm left) (arg_to_asm right)
   | ILabel(name) ->
-     name ^ ":"
+    name ^ ":"
   | IJo(label) ->
-     sprintf "  jo %s" label
+    sprintf "  jo %s" label
   | IJe(label) ->
-     sprintf "  je %s" label
+    sprintf "  je %s" label
   | IJne(label) ->
-     sprintf "  jne %s" label
+    sprintf "  jne %s" label
   | IJl(label) ->
-     sprintf "  jl %s" label
+    sprintf "  jl %s" label
   | IJle(label) ->
-     sprintf "  jle %s" label
+    sprintf "  jle %s" label
   | IJg(label) ->
-     sprintf "  jg %s" label
+    sprintf "  jg %s" label
   | IJge(label) ->
-     sprintf "  jge %s" label
+    sprintf "  jge %s" label
   | IJmp(label) ->
-     sprintf "  jmp %s" label
+    sprintf "  jmp %s" label
   | IJz(label) ->
-     sprintf "  jz %s" label
+    sprintf "  jz %s" label
   | IJnz(label) ->
-     sprintf "  jnz %s" label
+    sprintf "  jnz %s" label
   | IAnd(dest, value) ->
-     sprintf "  and %s, %s" (arg_to_asm dest) (arg_to_asm value)
+    sprintf "  and %s, %s" (arg_to_asm dest) (arg_to_asm value)
   | IOr(dest, value) ->
-     sprintf "  or %s, %s" (arg_to_asm dest) (arg_to_asm value)
+    sprintf "  or %s, %s" (arg_to_asm dest) (arg_to_asm value)
   | IXor(dest, value) ->
-     sprintf "  xor %s, %s" (arg_to_asm dest) (arg_to_asm value)
+    sprintf "  xor %s, %s" (arg_to_asm dest) (arg_to_asm value)
   | IShl(dest, value) ->
-     sprintf "  shl %s, %s" (arg_to_asm dest) (arg_to_asm value)
+    sprintf "  shl %s, %s" (arg_to_asm dest) (arg_to_asm value)
   | IShr(dest, value) ->
-     sprintf "  shr %s, %s" (arg_to_asm dest) (arg_to_asm value)
+    sprintf "  shr %s, %s" (arg_to_asm dest) (arg_to_asm value)
   | ISar(dest, value) ->
-     sprintf "  sar %s, %s" (arg_to_asm dest) (arg_to_asm value)
+    sprintf "  sar %s, %s" (arg_to_asm dest) (arg_to_asm value)
   | IPush(value) ->
-     sprintf "  push %s" (arg_to_asm value)
+    sprintf "  push %s" (arg_to_asm value)
   | IPop(dest) ->
-     sprintf "  pop %s" (arg_to_asm dest)
+    sprintf "  pop %s" (arg_to_asm dest)
   | ICall(label) ->
-     sprintf "  call %s" label
+    sprintf "  call %s" label
   | IRet ->
-     "  ret"
+    "  ret"
   | ITest(arg, comp) ->
-     sprintf "  test %s, %s" (arg_to_asm arg) (arg_to_asm comp)
+    sprintf "  test %s, %s" (arg_to_asm arg) (arg_to_asm comp)
   | ILineComment(str) ->
-     sprintf "  ;; %s" str
+    sprintf "  ;; %s" str
   | IInstrComment(instr, str) ->
-     sprintf "%s ; %s" (i_to_asm instr) str
+    sprintf "%s ; %s" (i_to_asm instr) str
 
 let to_asm (is : instruction list) : string =
   List.fold_left (fun s i -> sprintf "%s\n%s" s (i_to_asm i)) "" is
@@ -261,14 +261,14 @@ let rec find (ls : (string * 'a) list) (x : string) : 'a =
   match ls with
   | [] -> raise (InternalCompilerError (sprintf "Name %s not found" x))
   | (y,v)::rest ->
-     if y = x then v else find rest x
+    if y = x then v else find rest x
 
 (* NOTE: Assumes that e is in ANF *)
 let rec count_vars (e : 'a expr) =
   match e with
   | EIf(_, t, f, _) -> max (count_vars t) (count_vars f)
   | ELet([_, b, _], body, _) ->
-     1 + (max (count_vars b) (count_vars body))
+    1 + (max (count_vars b) (count_vars body))
   | _ -> 0
 
 let rec replicate (x : 'a) (i : int) : 'a list =
@@ -278,11 +278,11 @@ let rec replicate (x : 'a) (i : int) : 'a list =
 let rec compile_expr (e : tag expr) (si : int) (env : (string * int) list) : instruction list =
   match e with
   | ELet([id, e, _], body, _) ->
-     let prelude = compile_expr e (si + 1) env in
-     let body = compile_expr body (si + 1) ((id, si)::env) in
-     prelude
-     @ [ IMov(RegOffset(~-si, RBP), Reg(RAX)) ]
-     @ body
+    let prelude = compile_expr e (si + 1) env in
+    let body = compile_expr body (si + 1) ((id, si)::env) in
+    prelude
+    @ [ IMov(RegOffset(~-si, RBP), Reg(RAX)) ]
+    @ body
   | EPrim1 _ -> raise (NotYetImplemented "Fill in here")
   | EPrim2 _ -> raise (NotYetImplemented "Fill in here")
   | EIf _ -> raise (NotYetImplemented "Fill in here")
@@ -293,11 +293,11 @@ let rec compile_expr (e : tag expr) (si : int) (env : (string * int) list) : ins
 and compile_imm (e : tag expr) (env : (string * int) list) : arg =
   match e with
   | ENumber(n, _) ->
-     if n > (Int64.div Int64.max_int 2L) || n < (Int64.div Int64.min_int 2L) then
-       (* TODO: raise a better error of your choosing here *)
-       failwith ("Integer overflow: " ^ (Int64.to_string n))
-     else
-       raise (NotYetImplemented "Fill in here")
+    if n > (Int64.div Int64.max_int 2L) || n < (Int64.div Int64.min_int 2L) then
+      (* TODO: raise a better error of your choosing here *)
+      failwith ("Integer overflow: " ^ (Int64.to_string n))
+    else
+      raise (NotYetImplemented "Fill in here")
   | EBool(true, _) -> raise (NotYetImplemented "Fill in here")
   | EBool(false, _) -> raise (NotYetImplemented "Fill in here")
   | EId(x, _) -> RegOffset(~-(find env x), RBP)
@@ -311,11 +311,11 @@ extern error
 extern print
 global our_code_starts_here" in
   let stack_setup = [
-      (* FILL: insert instructions for setting up stack here *)
-    ] in
+    (* FILL: insert instructions for setting up stack here *)
+  ] in
   let postlude = [
-      IRet
-      (* FILL: insert instructions for cleaning up stack, and maybe
+    IRet
+    (* FILL: insert instructions for cleaning up stack, and maybe
        some labels for jumping to errors, here *) ] in
   let body = (compile_expr anfed 1 []) in
   let as_assembly_string = (to_asm (stack_setup @ body @ postlude)) in
