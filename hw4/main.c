bf29dd34e070178ff7e536eff362db2ac422c6b1
Conner Nilsen <connernilsen@gmail.com>
2022-02-14 13:22:30 -0500

    Add error handler in c runtime

--- hw4/main.c
+++ hw4/main.c
@@ -5,10 +5,35 @@
 
 extern uint64_t our_code_starts_here() asm("our_code_starts_here");
 extern uint64_t print(uint64_t val) asm("print");
+extern void error(int errCode, int val) asm("error");
 const uint64_t NUM_TAG_MASK = 0x0000000000000001L;
 const uint64_t BOOL_TAG_MASK = 0x0000000000000007L;
 const uint64_t TRUE = 0xFFFFFFFFFFFFFFFFL;
 const uint64_t FALSE = 0x7FFFFFFFFFFFFFFFL;
+const int COMP_NOT_NUM = 1;
+const int ARITH_NOT_NUM = 2;
+const int LOGIC_NOT_BOOL = 3;
+const int IF_NOT_BOOL = 4;
+const int OVERFLOW = 5;
+
+void error(int errCode, int val) {
+  if (errCode == COMP_NOT_NUM) {
+    fprintf(stderr, "Expected number type for comparison op, got %010x\n", val);
+  } else if (errCode == ARITH_NOT_NUM) {
+    fprintf(stderr, "Expected number type for arithmetic op, got %010x\n", val);
+  } else if (errCode == LOGIC_NOT_BOOL) {
+    fprintf(stderr, "Expected bool type for logical op, got %010x\n", val);
+  } else if (errCode == IF_NOT_BOOL) {
+    fprintf(stderr, "Expected bool type for if stmt, got %010x\n", val);
+  } else if (errCode == OVERFLOW) {
+    fprintf(stderr, "Overflow occurred for arithmetic operation, got %010x\n", val);
+  } else {
+    fprintf(stderr, "Unknown error code provided (%d) for value %010x\n", 
+        errCode, val);
+  }
+
+  exit(errCode);
+}
 
 uint64_t print(uint64_t val)
 {
