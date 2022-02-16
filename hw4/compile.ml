8d3894d77d88216d70f282f13a91267a0291108a
Kyle <kyleatyahoo@gmail.com>
2022-02-16 14:25:40 -0500

    num_tag_mask

--- hw4/compile.ml
+++ hw4/compile.ml
@@ -373,7 +373,7 @@ let rec compile_expr (e : tag expr) (si : int) (env : (string * int) list) : ins
       | IsNum ->
         let label_not_num = (sprintf "%s%n" label_IS_NOT_NUM tag) in 
         let label_done = (sprintf "%s%n" label_DONE tag) in
-        (create_type_check bool_tag_mask label_not_num true 
+        (create_type_check num_tag_mask label_not_num true 
         [
           IMov(Reg(RAX), const_true);
           IJmp(label_done);
