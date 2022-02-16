8189501a1687736e3aa72756fa5973984c8edc9b
Kyle <kyleatyahoo@gmail.com>
2022-02-16 12:15:48 -0500

    fix print

--- hw4/compile.ml
+++ hw4/compile.ml
@@ -359,12 +359,7 @@ let rec compile_expr (e : tag expr) (si : int) (env : (string * int) list) : ins
         IMov(Reg(RAX), e_reg) ::
         (create_type_check num_tag_mask label_ARITH_NOT_NUM true
           [IAdd(Reg(RAX), Const(Int64.neg 2L))])
-<<<<<<< HEAD
-        (* TODO: is e already available in e_reg? *)
-      | Print -> compile_expr e (si + 1) env @ (setup_func_call [Reg(RAX)] "print")
-=======
-      | Print -> IMov(Reg(RAX), e_reg) :: (setup_func_call [e_reg] "print")
->>>>>>> temp
+      | Print -> (IMov(Reg(RAX), e_reg) :: (setup_func_call [e_reg] "print"))
       | IsBool -> 
         let label_not_bool = (sprintf "%s%n" label_IS_NOT_BOOL tag) in 
         let label_done = (sprintf "%s%n" label_DONE tag) in
